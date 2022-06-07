e21f6ac0ca60ad8d8bc6ba0eeb5be101c1457666
ing-arriola <ingeniero.arriola@gmail.com>
2022-06-06 20:35:59 -0600

    claseLunes6

--- KATA_INTRO_FRONT/app-counter/src/App.js
+++ KATA_INTRO_FRONT/app-counter/src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
+
+import Button from './components/Button'
+import InputValue from './components/InputValue'
 import logo from "./logo.svg";
 import "./App.css";
-//holas
+
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       contador: 0,
+      limiteSuperior:10,
+      limiteInferior:3
     };
     this.aumentarContador = this.aumentarContador.bind(this);
+    this.decreaseCounter = this.decreaseCounter.bind(this);
+    this.setUpperLimit = this.setUpperLimit.bind(this);
+    this.setLowerLimit = this.setLowerLimit.bind(this);
   }
 
   aumentarContador() {
-    if (this.state.contador < 10) {
+    if (this.state.contador < this.state.limiteSuperior) {
       this.setState({ contador: this.state.contador + 1 });
     }
   }
 
   decreaseCounter() {
-    if (this.state.contador > 0) {
+    if (this.state.contador > this.state.limiteInferior) {
       this.setState({ contador: this.state.contador - 1 });
     }
+  }
+
+  setUpperLimit(upperLimit){
+    this.setState({...this.state,limiteSuperior:upperLimit})
+  }
+
+  setLowerLimit(lowerLimit){
+    this.setState({...this.state,limiteInferior:lowerLimit})
   }
 
   render() {
@@ -29,9 +45,22 @@ export default class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h3>Contador</h3>
+          <p> {JSON.stringify(this.state)} </p>
+          <InputValue 
+            limite={this.state.limiteSuperior} 
+            setLimit={this.setUpperLimit}
+            title='Limite superior'
+          />
+          <InputValue 
+            limite={this.state.limiteInferior} 
+            setLimit={this.setLowerLimit}
+            title='Limite Inferior'
+          />
+          
           <p>{this.state.contador}</p>
-          <button onClick={this.aumentarContador}>Incrementar</button>
-          <button onClick={() => this.decreaseCounter()}>Decrease</button>
+          
+          <Button handleClick={this.aumentarContador} title='Go up' />
+          <Button handleClick={this.decreaseCounter} title='Go down' />
         </header>
       </div>
     );
