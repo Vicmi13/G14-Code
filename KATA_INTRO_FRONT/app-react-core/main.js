7446102c8dd1d75de8fe170f97fadf72543758bb
Victor Torres Valencia <victor.tv@wizeline.com>
2022-06-09 18:35:20 -0500

    Added projet understanding_react

---
+++ KATA_INTRO_FRONT/app-react-core/main.js
@@ -0,0 +1,20 @@
+const root = document.getElementById("div-root");
+const element = document.createElement("div");
+
+
+// MANIPULACION DEL DOM
+element.textContent = "Hello world DOM";
+element.className = "container";
+root.appendChild(element);
+
+
+/** APLICADO CON REACT */
+
+/* 
+ *
+ * SINTAXIS React.createElement(typo_de_elemento, { children },  valor)
+*/
+
+
+
+
