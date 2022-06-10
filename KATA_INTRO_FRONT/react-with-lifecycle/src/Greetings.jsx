21eb813fda4fb8c9bd7f45b55e12ba7b529093ab
Victor Torres Valencia <victor.tv@wizeline.com>
2022-06-09 21:03:35 -0500

    Added componentWillUnmount

--- KATA_INTRO_FRONT/react-with-lifecycle/src/Greetings.jsx
+++ KATA_INTRO_FRONT/react-with-lifecycle/src/Greetings.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export default class Greetings extends Component {
+  componentWillUnmount() {
+    alert("Se va a remover el componente");
+  }
+
   render() {
     return <div>Saludos eres mayor de edad</div>;
   }
