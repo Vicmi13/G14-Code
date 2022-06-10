288b9104b9bc46b586611b387203348f84d08dab
Victor Torres Valencia <victor.tv@wizeline.com>
2022-06-09 19:36:39 -0500

    Create new project for lifecycle react

---
+++ KATA_INTRO_FRONT/react-with-lifecycle/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hoy veremos ciclo de vida & conditional rendering.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
